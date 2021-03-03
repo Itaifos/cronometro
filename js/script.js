3ce9f7cf85ae64e9947ebe5d5e4115f5378cb7dc
Itaifos <itaifos@gmail.com>
2021-03-03 18:02:50 -0300

    novas funções

--- js/script.js
+++ js/script.js
@@ -1,36 +1,70 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', () => {
+    
+    createButtonStart();
+    createButtonPause();
+});
+
+function clock() {
+
     const timer = document.querySelector('#timer');
 
-    let hour = 0;
-    let minute = 0;
-    let second = 0;
+    let hour = "0" + 0;
+    let minute = "0" + 0;
+    let second = "0" + 0;
     let millisecond = 0;
 
-    function clock() {
-
-        setInterval(() => {
-            
-            if (millisecond === 1000) {
-                second++;
-                millisecond = 0;
+    setInterval(() => {
+        
+        if (millisecond === 1000) {
+            second++;
+            if (second < 10) {
+                second = '0' + second;
+                second = second.substr(0,2);
             }
+            millisecond = 0;
+        }
 
-            if (second === 60) {
-                minute++;
-                second = 0;
+        if (second === 60) {
+            minute++;
+            if (minute < 10) {
+                minute = '0' + minute;
+                minute = minute.substr(0,2);
             }
-            if (minute === 60) {
-                hour++;
-                minute = 0;
+            second = 0;
+        }
+        if (minute === 60) {
+            hour++;
+            if (hour < 10) {
+                hour = '0' + hour;
+                hour = hour.substr(0,2);
             }
-            
-            millisecond = millisecond + 10;
-            timer.textContent = hour + ':' + minute + ':' + second + ':' + millisecond;
-        }, 10);
+            minute = 0;
+        }
+        
+        millisecond = millisecond + 10;
+        
+        timer.innerHTML = hour + ':' + minute + ':' + second + ':' + millisecond;
+    }, 10);
+}
 
+function createButtonStart() {
 
+    let buttonStart = document.createElement('button');
+    buttonStart.innerHTML = 'Start';
+    buttonStart.addEventListener('click', clock);
+    timerButtons.appendChild(buttonStart);
+
+}
+
+function createButtonPause() {
+
+    function myclearInterval() {
+        clearInterval(clock);
     }
-    
-    clock();
-    
-});
+
+    let buttonPause = document.createElement('button');
+    buttonPause.innerHTML = 'Pause';
+    buttonPause.addEventListener('click', myclearInterval);
+    timerButtons.appendChild(buttonPause);
+
+}
